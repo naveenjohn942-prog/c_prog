84245e0c10b22c3f9e53b45b87d9959df8e4d483
Naveen John <65065036+naveenjohn942-prog@users.noreply.github.com>
2023-09-30 11:41:21 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,193 +1,262 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
-struct subscriber   //structure for subscriber data
-{
-char phonenumber[20];
-char name[50];
-float amount;
-}s;
-void addrecords();  //function for adding records
-void listrecords();  //function for list of record
-void modifyrecords();  // function for modifying the record
-void searchrecords();    //function for searching the record
-void payment();  //function for updating payment of individual subscriber
-char get;
-int main()  //main function
-{
-int phonenumber;
-int choice;
-printf("\n\n\n\n\n\n\n\n\n**************************************************************");
-printf("\n\t\t------WELCOME TO THE TELECOM BILLING MANAGEMENT SYSTEM---");
-printf("\n\t\t****************************************************************");
-while (choice!=6)
-{
-printf("\n Enter\n 1 : for adding new records.\n 2 : for list of records");
-printf("\n 3 : for modifying records.\n 4 : for payment");
-printf("\n 5 : for searching records.");
-printf("\n 6 : for exit\n");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:
-addrecords();break;
-case 2:
-listrecords();break;
-case 3:
-modifyrecords();break;
-case 4:
-payment();break;
-case 5:
-searchrecords();break;
-case 6:
-printf("\n\n\t\t\t\tTHANK YOU FOR USING OUR SERVICE");
-exit(0);
-break;
-default:
-printf("Incorrect Input");
-printf("\nAny key to continue");
-break;
-}
-}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define PHONE_NUMBER_LENGTH 10
+#define MAX_NAME_LENGTH 50
+#define FILE_NAME "subscriber_data.dat"
+
+struct subscriber {
+    char phonenumber[PHONE_NUMBER_LENGTH + 1]; // +1 for null terminator
+    char name[MAX_NAME_LENGTH];
+    float amount;
+};
+
+// Function prototypes
+void addRecord();
+void listRecords();
+void modifyRecord();
+void searchRecord();
+void makePayment();
+int isPhoneNumberValid(const char *phone);
+int isStringValid(const char *str);
+
+int main() {
+    int choice;
+
+    printf("\n\n\n\n\n\n\n\n\n**************************************************************");
+    printf("\n\t\t------WELCOME TO THE TELECOM BILLING MANAGEMENT SYSTEM---");
+    printf("\n\t\t****************************************************************");
+
+    while (1) {
+        printf("\n Menu:\n 1: Add new record\n 2: List records\n 3: Modify record\n 4: Make payment\n 5: Search records\n 6: Exit\n");
+        printf(" Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addRecord();
+                break;
+            case 2:
+                listRecords();
+                break;
+            case 3:
+                modifyRecord();
+                break;
+            case 4:
+                makePayment();
+                break;
+            case 5:
+                searchRecord();
+                break;
+            case 6:
+                printf("\n\n\t\t\t\tTHANK YOU FOR USING OUR SERVICE\n");
+                exit(0);
+            default:
+                printf("Invalid input. Please try again.\n");
+                break;
+        }
+    }
+
+    return 0;
 }
 
-void addrecords()
-{
-FILE *f;
-int test=1;
-f=fopen("file.ojs","ab+");
-if(f==0)
-{   f=fopen("file.ojs","wb+");
-printf("please wait while we configure your computer");
-printf("/npress any key to continue");
-}
-while(test!=27)
-{
-printf("\n Enter phone number:");
-scanf("%s",s.phonenumber);
-printf("\n Enter name:");
-scanf("%s[^\n]",s.name);
-printf("\n Enter amount:");
-scanf("%f",&s.amount);
-fwrite(&s,sizeof(s),1,f);
-printf("1 record successfully added");
-printf("\n Press 27 to exit, any other key to add other record:");
-scanf("%d",&test);
-if(test==27)
-break;
-}
-fclose(f);
+int isPhoneNumberValid(const char *phone) {
+    // Check if the phone number has exactly 10 digits
+    return (strlen(phone) == PHONE_NUMBER_LENGTH && strspn(phone, "0123456789") == PHONE_NUMBER_LENGTH);
 }
 
-void listrecords()
-{
-FILE *f;
-int i;
-if((f=fopen("file.ojs","rb"))==NULL)
-exit(0);
-system("cls");
-printf("Phone Number\t\tUser Name\t\t\tAmount\n");
-for(i=0;i<79;i++)
-printf("-");
-while(fread(&s,sizeof(s),1,f)==1)
-{
-printf("\n%-10s\t\t%-20s\t\tRs. %.2f /-",s.phonenumber,s.name,s.amount);
-}
-printf("\n");
-for(i=0;i<79;i++)
-printf("-");
-fclose(f);
+void addRecord() {
+    FILE *file = fopen(FILE_NAME, "ab+");
+    if (file == NULL) {
+        printf("Error opening the data file.\n");
+        exit(1);
+    }
+
+    struct subscriber s;
+
+    printf("\nEnter phone number (10 digits only): ");
+    scanf("%s", s.phonenumber);
+    if (!isPhoneNumberValid(s.phonenumber)) {
+        printf("Invalid phone number format. Please enter 10 digits.\n");
+        fclose(file);
+        return;
+    }
+
+    printf("Enter name: ");
+    scanf(" %[^\n]s", s.name); // Read the name with spaces
+    if (strlen(s.name) >= MAX_NAME_LENGTH) {
+        printf("Name is too long. Maximum name length is %d characters.\n", MAX_NAME_LENGTH - 1);
+        fclose(file);
+        return;
+    }
+
+    printf("Enter amount: ");
+    scanf("%f", &s.amount);
+
+    fwrite(&s, sizeof(struct subscriber), 1, file);
+
+    fclose(file);
+    printf("Record successfully added.\n");
 }
 
-void searchrecords()
-{
-FILE *f;
-char phonenumber[20];
-int flag=1;
-f=fopen("file.ojs","rb+");
-if(f==0)
-exit(0);
-fflush(stdin);
-printf("Enter Phone Number to search in our database");
-scanf("%s", phonenumber);
-while(fread(&s,sizeof(s),1,f)==1)
-{
-if(strcmp(s.phonenumber,phonenumber)==0)
-{
-printf(" Record Found ");
-printf("\n\nPhonenumber: %s\nName: %s\nAmount: Rs.%0.2f\n",s.phonenumber,s.name,s.amount);
-flag=0;
-break;
-}
-else if(flag==1)
-{
-printf("Requested Phone Number Not found in our database");
-}
-}
-fclose(f);
+void listRecords() {
+    FILE *file = fopen(FILE_NAME, "rb");
+    if (file == NULL) {
+        printf("Error opening the data file.\n");
+        exit(1);
+    }
+
+    struct subscriber s;
+    printf("\nPhone Number\t\tUser Name\t\tAmount\n");
+    printf("----------------------------------------------\n");
+
+    while (fread(&s, sizeof(struct subscriber), 1, file)) {
+        printf("%-20s%-20sRs. %.2f\n", s.phonenumber, s.name, s.amount);
+    }
+
+    fclose(file);
 }
 
-void modifyrecords()
-{
-FILE *f;
-char phonenumber[20];
-long int size=sizeof(s);
-if((f=fopen("file.ojs","rb+"))==NULL)
-exit(0);
-printf("Enter phone number of the subscriber to modify:");
-scanf("%s[^\n]",phonenumber);
-fflush(stdin);
-while(fread(&s,sizeof(s),1,f)==1)
-{
-if(strcmp(s.phonenumber,phonenumber)==0)
-{
-printf("\n Enter phone number:");
-scanf("%s",s.phonenumber);
-printf("\n Enter name: ");
-fflush(stdin);
-scanf("%s[^\n]",s.name);
-printf("\n Enter amount: ");
-scanf("%f",&s.amount);
-fseek(f,-size,SEEK_CUR);
-fwrite(&s,sizeof(s),1,f);
-break;
-}
-}
-fclose(f);
+void searchRecord() {
+    char phonenumber[PHONE_NUMBER_LENGTH + 1];
+    printf("\nEnter phone number to search (10 digits only): ");
+    scanf("%s", phonenumber);
+    if (!isPhoneNumberValid(phonenumber)) {
+        printf("Invalid phone number format. Please enter 10 digits.\n");
+        return;
+    }
+
+    FILE *file = fopen(FILE_NAME, "rb");
+    if (file == NULL) {
+        printf("Error opening the data file.\n");
+        exit(1);
+    }
+
+    struct subscriber s;
+    int found = 0;
+
+    while (fread(&s, sizeof(struct subscriber), 1, file)) {
+        if (strcmp(s.phonenumber, phonenumber) == 0) {
+            printf("\nRecord Found:\n");
+            printf("Phone Number: %s\nName: %s\nAmount: Rs. %.2f\n", s.phonenumber, s.name, s.amount);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("\nRequested Phone Number not found in our database.\n");
+    }
+
+    fclose(file);
 }
 
-void payment()
-{
-FILE *f;
-char phonenumber[20];
-long int size=sizeof(s);
-float amt;
-int i;
-if((f=fopen("file.ojs","rb+"))==NULL)
-exit(0);
-printf("Enter phone number of the subscriber for payment");
-scanf("%s[^\n]",phonenumber);
-fflush(stdin);
-while(fread(&s,sizeof(s),1,f)==1)
-{
-if(strcmp(s.phonenumber,phonenumber)==0)
-{
-printf("\n Phone No.: %s",s.phonenumber);
-printf("\n Name: %s",s.name);
-printf("\n Current amount: %f",s.amount);
-printf("\n");
-for(i=0;i<79;i++)
-printf("-");
-printf("\n\nEnter amount of payment :");
-fflush(stdin);
-scanf(" %f",&amt);
-s.amount=s.amount-amt;
-fseek(f,-size,SEEK_CUR);
-fwrite(&s,sizeof(s),1,f);
-break;
+void modifyRecord() {
+    char phonenumber[PHONE_NUMBER_LENGTH + 1];
+    printf("\nEnter phone number to modify (10 digits only): ");
+    scanf("%s", phonenumber);
+    if (!isPhoneNumberValid(phonenumber)) {
+        printf("Invalid phone number format. Please enter 10 digits.\n");
+        return;
+    }
+
+    FILE *file = fopen(FILE_NAME, "rb+");
+    if (file == NULL) {
+        printf("Error opening the data file.\n");
+        exit(1);
+    }
+
+    struct subscriber s;
+    int found = 0;
+
+    long int offset = -sizeof(struct subscriber); // Define offset as long int
+
+    while (fread(&s, sizeof(struct subscriber), 1, file)) {
+        if (strcmp(s.phonenumber, phonenumber) == 0) {
+            printf("\nEnter new phone number (10 digits only): ");
+            scanf("%s", s.phonenumber);
+            if (!isPhoneNumberValid(s.phonenumber)) {
+                printf("Invalid phone number format. Please enter 10 digits.\n");
+                fclose(file);
+                return;
+            }
+
+            printf("Enter new name: ");
+            scanf(" %[^\n]s", s.name); // Read the name with spaces
+            if (strlen(s.name) >= MAX_NAME_LENGTH) {
+                printf("Name is too long. Maximum name length is %d characters.\n", MAX_NAME_LENGTH - 1);
+                fclose(file);
+                return;
+            }
+
+            printf("Enter new amount: ");
+            scanf("%f", &s.amount);
+
+            fseek(file, offset, SEEK_CUR);
+            fwrite(&s, sizeof(struct subscriber), 1, file);
+
+            printf("Record successfully modified.\n");
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("\nRequested Phone Number not found in our database.\n");
+    }
+
+    fclose(file);
 }
+
+void makePayment() {
+    char phonenumber[PHONE_NUMBER_LENGTH + 1];
+    printf("\nEnter phone number for payment: ");
+    scanf("%s", phonenumber);
+
+    if (!isPhoneNumberValid(phonenumber)) {
+        printf("Invalid phone number format. Please enter 10 digits.\n");
+        return;
+    }
+
+    FILE *file = fopen(FILE_NAME, "rb+");
+    if (file == NULL) {
+        printf("Error opening the data file.\n");
+        exit(1);
+    }
+
+    struct subscriber s;
+    int found = 0;
+
+    long int offset = -sizeof(struct subscriber); // Define offset as long int
+
+    while (fread(&s, sizeof(struct subscriber), 1, file)) {
+        if (strcmp(s.phonenumber, phonenumber) == 0) {
+            printf("\nPhone Number: %s\nName: %s\nCurrent Amount: Rs. %.2f\n", s.phonenumber, s.name, s.amount);
+            float paymentAmount;
+            printf("Enter payment amount: ");
+            scanf("%f", &paymentAmount);
+
+            if (paymentAmount <= s.amount) {
+                s.amount -= paymentAmount;
+                fseek(file, offset, SEEK_CUR);
+                fwrite(&s, sizeof(struct subscriber), 1, file);
+
+                printf("Payment successful. Updated amount: Rs. %.2f\n", s.amount);
+            } else {
+                printf("Payment amount exceeds the current balance. Payment not accepted.\n");
+            }
+
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("\nRequested Phone Number not found in our database.\n");
+    }
+
+    fclose(file);
 }
-printf("THANK YOU %s FOR YOUR TIMELY PAYMENTS",s.name);
-fclose(f);
-}
+
